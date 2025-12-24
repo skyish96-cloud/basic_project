33ab2363833d60dd5581a7064c09e5df78023c62
김기영 <skyish96@gmail.com>
2025-12-24 15:51:40 +0900

    step_one

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ app.mount("/view", StaticFiles(directory="view"), name="view")
 @app.get("/")
 def main():
     return RedirectResponse("/view/index.html")
+
+@app.get("/one")
+def step_one():
+    return {"step":"one"}
